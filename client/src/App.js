4f5ca39bbe29271ad60138baff04600424044a76
Nick Filatov <keddofilatov@gmail.com>
2018-05-24 23:40:21 +0600

    [+] loadMore

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider, Query } from "react-apollo";
 import gql from "graphql-tag";
+import styled from 'styled-components';
 
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql"
@@ -17,30 +18,56 @@ const GET_ALL_MOVIES = gql`
   }
 `;
 
-export const App = () => (
-  <ApolloProvider client={client}>
-    <Query query={GET_ALL_MOVIES} variables={{ page: 1 }}>
-      {({ loading, data: { allMovies }, error }) => {
-        if (loading) return <div>Loading</div>;
-        if (error) return <div>Error</div>;
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      page: 1
+    }
+  }
 
-        return (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-              flexWrap: "wrap"
-            }}
-          >
-            {allMovies.map(movie => (
-              <MoviePreview movie={movie} key={movie.id} />
-            ))}
-          </div>
-        );
-      }}
-    </Query>
-  </ApolloProvider>
-);
+  render() {
+    return (<ApolloProvider client={client}>
+      <Query query={GET_ALL_MOVIES} variables={{ page: this.state.page }}>
+        {({ loading, data: { allMovies = [] }, error, fetchMore, variables: { page } }) => {
+          let content;
+          if (loading) { content = <div>Loading</div> } else
+            if (error) { content = <div>Error</div> } else {
+              content = (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    flexWrap: "wrap",
+                    marginTop: '35px',
+                  }}
+                >
+                  {allMovies.map(movie => (
+                    <MoviePreview movie={movie} key={movie.id} />
+                  ))}
+                </div>
+              )
+            }
+          return (
+            <div style={{ margin: '0 auto' }}>
+              <Button onClick={() => this.setState({ page: page === 1 ? page : page - 1 })}>PREV</Button>
+              <Button onClick={() => this.setState({ page: page + 1 })}>NEXT</Button>
+              {content}
+            </div>
+          );
+        }}
+      </Query>
+    </ApolloProvider>)
+  }
+}
+
+const Button = styled.button`
+  width: 50%;
+  height: 50px;
+  cursor: pointer;
+  color: white;
+  background-color: #444456;
+`;
 
 const MoviePreview = ({ movie: { poster, title } }) => (
   <div>
